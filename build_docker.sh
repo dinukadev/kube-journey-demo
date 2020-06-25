b2dfe1f43595ad232da9ec646003b55cc6b1a330
dinukadev <dinuka.arseculeratne@gmail.com>
2020-06-25 17:09:05 +1000

    add ecr login

--- build_docker.sh
+++ build_docker.sh
@@ -6,7 +6,7 @@ MS_NAME=ms-kubejourney
 TAG=dev
 
 
-
+$(aws ecr get-login --no-include-email --region ap-southeast-2)
 aws ecr batch-delete-image --repository-name $MS_NAME --image-ids imageTag=latest-$TAG --region $REGION
 docker build --force-rm=true -t $MS_NAME:latest .
 docker tag $MS_NAME:latest $ECR_REPO/$MS_NAME:latest-$TAG
